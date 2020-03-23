2e1ffd7ade3935cddc6c7f84164969ef4635e519
Rowan2005 <58427428+Rowan2005@users.noreply.github.com>
2020-03-23 22:07:33 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,6 @@
+class Player {
+    constructor(){
+      this.name = null;
+      this.score = 0;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C-47-Projects
-Projects
+# p5.play-boilerplate
+Boiler plate for p5.play
